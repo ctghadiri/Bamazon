e9eb43a1733284554cc3dcc283f11d4a25bd94b3
ctghadiri <ctghadiri@gmail.com>
2019-03-25 15:47:49 -0700

    add connection to sql and add requirements for mysql and inquirer

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_DB"
+});
+connection.connect(function(err){
+    if (err) throw err;
+});
