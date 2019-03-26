fae36b7556e5e8ece65ac1f407508273544fe831
ctghadiri <ctghadiri@gmail.com>
2019-03-26 15:08:58 -0700

    add .gitignore add create database and use for sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,12 +1,14 @@
 DROP DATABASE IF EXISTS bamazon_DB;
 
-CREATE bamazon_DB;
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB
 
 CREATE TABLE products(
     item_id INT AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR NOT NULL
-    department_name VARCHAR NOT NULL
-    price DECIMAL (100,2) NOT NULL,
+    product_name VARCHAR(50) NOT NULL
+    department_name VARCHAR(50) NOT NULL
+    price DECIMAL (50,2) NOT NULL,
     stock INTEGER NOT NULL,
     PRIMARY KEY (item_id)
 );
