795446a04936bfbfc3129dad1c245bea8db81a58
ctghadiri <ctghadiri@gmail.com>
2019-03-26 09:39:29 -0700

    add pull for item ids and work on inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,4 +10,32 @@ var connection = mysql({
 });
 connection.connect(function(err){
     if (err) throw err;
+
 });
+var itemID
+connection.query("SELECT item_id FROM products", function (err, data){
+    if(err) throw err;
+    itemID = res.item_id;
+    console.log(itemID);
+    return itemID
+})
+inquirer
+.prompt(
+    {
+        name: 'itemId',
+        type: 'input',
+        message: 'What is the ID of the product they would like to buy?',
+        validate: function validateID(input){
+            if(input === ){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    },
+    {
+        name:'quantity',
+        type: ''
+    }
+)
