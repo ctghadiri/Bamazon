e96250fd783057842b380d3c3cacb98fdb4d86e2
ctghadiri <ctghadiri@gmail.com>
2019-03-26 15:47:01 -0700

    debug msql connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-var connection = mysql({
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
