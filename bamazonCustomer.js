8c2ce6871ed8fe4f0a0e208022614799a9151469
ctghadiri <ctghadiri@gmail.com>
2019-03-26 17:13:03 -0700

    require cli table and create data pull with function for inputing into cli table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table2')
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -19,6 +20,22 @@ connection.query("SELECT item_id FROM products", function (err, data){
     question();
 })
 
+connection.query("SELECT * FROM products"), function (err, res){
+    if (err) throw err;
+    var res = res
+    console.log(res);
+    var table = new Table({
+        head: ['item_id', 'product_name', 'department_name', 'price', 'stock'], colWidths: [100, 200]
+    });
+
+    for(var i = 0, i < res.length; i++){
+
+        table.push(res[i]);
+
+    }
+    console.log(table.toString());
+}
+
 function question(){
     inquirer
     .prompt(
@@ -27,7 +44,7 @@ function question(){
             type: 'input',
             message: 'What is the ID of the product they would like to buy?',
             validate: function validateID(input){
-                if(input <= itemID.length){
+                if(input <= itemID.length && input >0){
                     return true;
                 }
                 else{
