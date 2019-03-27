bd8ce72e1022a55ec0279df0cfad0b19a74c205f
ctghadiri <ctghadiri@gmail.com>
2019-03-27 09:42:39 -0700

    debug the cli table still needs adjustment

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,72 +11,77 @@ var connection = mysql.createConnection({
 });
 connection.connect(function(err){
     if (err) throw err;
+    showTable();
 });
 
 // Make global variable for item chosen
-var itemChosen;
-// Function for question choosing item
-function chooseItem(){
-    inquirer
-    .prompt(
-        {
-            name: 'itemId',
-            type: 'input',
-            message: 'What is the ID of the product they would like to buy?',
-            validate: function validateID(input){
-                if(input <= itemID.length && input >0){
-                    itemChosen = input;
-                    return true;
-                }
-                else{
-                    console.log("\nPlease enter and ID from the list.")
-                    return false;
-                }
-            }
-        }
-    ).then(function (itemChosen){
-        chooseAmount();
-    });
-};
+// var itemChosen;
+// // Function for question choosing item
+// function chooseItem(){
+//     inquirer
+//     .prompt(
+//         {
+//             name: 'itemId',
+//             type: 'input',
+//             message: 'What is the ID of the product they would like to buy?',
+//             validate: function validateID(input){
+//                 if(input <= itemID.length && input >0){
+//                     itemChosen = input;
+//                     return true;
+//                 }
+//                 else{
+//                     console.log("\nPlease enter and ID from the list.")
+//                     return false;
+//                 }
+//             }
+//         }
+//     ).then(function (answer){
+//         chooseAmount();
+//     });
+// };
 
 // Make global variable for quantityChosen
-var quantityChosen;
-// Function for choosing amount of item chosen
-function chooseAmount(){
-    inquirer
-    .prompt(
-        {
-            name:'quantity',
-            type: 'input',
-            message: 'How many would you like to purchase?',
-            validate: function validateQuantity(input){
-                if(input >= productQuantity && input > 0){
-                    quantityChosen = input;
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-        }
-    );
-};
+// var quantityChosen;
+
+// // Function for choosing amount of item chosen
+// function chooseAmount(){
+//     inquirer
+//     .prompt(
+//         {
+//             name:'quantity',
+//             type: 'input',
+//             message: 'How many would you like to purchase?',
+//             validate: function validateQuantity(input){
+//                 if(input >= productQuantity && input > 0){
+//                     quantityChosen = input;
+//                     return true;
+//                 }
+//                 else{
+//                     return false;
+//                 }
+//             }
+//         }
+//     );
+// };
 
 // Creation of product list
-connection.query("SELECT * FROM products"), function (err, res){
-    if (err) throw err;
-    console.log(res)
-    
-//     var table = new Table({
-//         head: ['item_id', 'product_name', 'department_name', 'price', 'stock'], colWidths: [100, 200]
-//     });
+function showTable(){
 
-//     for(var i = 0;i < res.length; i++){
+    connection.query("SELECT * FROM products", function (err, res){
+        if (err) throw err; 
+        
+        var table = new Table({
+            head: ['item_id', 'product_name', 'department_name', 'price', 'stock'], colWidths: [100, 200],
+            colWidths: [50, 120, 120, 50, 50]
+        });
 
-//         table.push(res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock)
+        for(var i = 0;i < res.length; i++){
 
-//     }
-//     console.log(table.toString());
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock])
+
+        }
+        console.log(table.toString());
+    });
 }
 
 
@@ -89,25 +94,25 @@ connection.query("SELECT * FROM products"), function (err, res){
 // })
 
 // Pull for quantity amount
-var productQuantity;
-connection.query("SELECT stock FROM products WHERE ?",{
-    item_id: name.itemID
-}, function (err, res){
-    if(err) throw err;
-    productQuantity = res;
-})
+// var productQuantity;
+// connection.query("SELECT stock FROM products WHERE ?",{
+//     item_id: name.itemID
+// }, function (err, res){
+//     if(err) throw err;
+//     productQuantity = res;
+// })
 
 // Update table with new quantity
-connection.query("UPDATE products SET ? WHERE ?",
-    [
-        {
-            stock: answer.quantity
-        },
-        {
-            item_id: itemID
-        }
-    ], function(err, res){
-        if(err) throw err;
-    })
+// connection.query("UPDATE products SET ? WHERE ?",
+//     [
+//         {
+//             stock: answer.quantity
+//         },
+//         {
+//             item_id: itemID
+//         }
+//     ], function(err, res){
+//         if(err) throw err;
+//     })
 
 
