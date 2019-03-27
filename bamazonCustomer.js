8f4ba72c85aa7667b670c63efc6919f739a22780
ctghadiri <ctghadiri@gmail.com>
2019-03-27 15:45:54 -0700

    move quantity question in to the initial prompt and add slash in.gitignore

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,54 +15,48 @@ connection.connect(function(err){
 });
 
 // Make global variable for item chosen
-// var itemChosen;
-// // Function for question choosing item
-// function chooseItem(){
-//     inquirer
-//     .prompt(
-//         {
-//             name: 'itemId',
-//             type: 'input',
-//             message: 'What is the ID of the product they would like to buy?',
-//             validate: function validateID(input){
-//                 if(input <= itemID.length && input >0){
-//                     itemChosen = input;
-//                     return true;
-//                 }
-//                 else{
-//                     console.log("\nPlease enter and ID from the list.")
-//                     return false;
-//                 }
-//             }
-//         }
-//     ).then(function (answer){
-//         chooseAmount();
-//     });
-// };
+var itemChosen;
+// Function for question choosing item and quantity
+function chooseItem(res){
+    inquirer
+    .prompt(
+        {
+            name: 'itemId',
+            type: 'input',
+            message: 'What is the ID of the product they would like to buy? (Press q or Q to quit)',
+            validate: function validateID(input){
+                if(input <= res.length && input >0){
+                    itemChosen = input;
+                    return true;
+                }
+                else if(input === 'q' || input === 'Q'){
+                    connection.end();
+                }
+                else{
+                    console.log("\nPlease enter and ID from the list.")
+                    return false;
+                }
+            }
+        },
+        {
+            name:'quantity',
+            type: 'input',
+            message: 'How many would you like to purchase?',
+            validate: function validateQuantity(input){
+                if(input >= res && input > 0){
+                    quantityChosen = input;
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    ).then(function (answer){
 
-// Make global variable for quantityChosen
-// var quantityChosen;
-
-// // Function for choosing amount of item chosen
-// function chooseAmount(){
-//     inquirer
-//     .prompt(
-//         {
-//             name:'quantity',
-//             type: 'input',
-//             message: 'How many would you like to purchase?',
-//             validate: function validateQuantity(input){
-//                 if(input >= productQuantity && input > 0){
-//                     quantityChosen = input;
-//                     return true;
-//                 }
-//                 else{
-//                     return false;
-//                 }
-//             }
-//         }
-//     );
-// };
+        // chooseAmount();
+    });
+};
 
 // Creation of product list
 function showTable(){
@@ -81,38 +75,40 @@ function showTable(){
 
         }
         console.log(table.toString());
+
+        chooseItem(res);
     });
 }
 
 
 // Pull for array of items
-// var itemID;
-// connection.query("SELECT item_id FROM products", function (err, res){
-//     if(err) throw err;
-//     itemID = res;
-//     // chooseItem();
-// })
+var itemID;
+connection.query("SELECT item_id FROM products", function (err, res){
+    if(err) throw err;
+    itemID = res;
+    // chooseItem();
+})
 
 // Pull for quantity amount
-// var productQuantity;
-// connection.query("SELECT stock FROM products WHERE ?",{
-//     item_id: name.itemID
-// }, function (err, res){
-//     if(err) throw err;
-//     productQuantity = res;
-// })
+var productQuantity;
+connection.query("SELECT stock FROM products WHERE ?",{
+    item_id: name.itemID
+}, function (err, res){
+    if(err) throw err;
+    productQuantity = res;
+})
 
 // Update table with new quantity
-// connection.query("UPDATE products SET ? WHERE ?",
-//     [
-//         {
-//             stock: answer.quantity
-//         },
-//         {
-//             item_id: itemID
-//         }
-//     ], function(err, res){
-//         if(err) throw err;
-//     })
+connection.query("UPDATE products SET ? WHERE ?",
+    [
+        {
+            stock: answer.quantity
+        },
+        {
+            item_id: itemID
+        }
+    ], function(err, res){
+        if(err) throw err;
+    })
 
 
